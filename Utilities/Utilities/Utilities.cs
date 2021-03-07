728e338f187f20189278dd9cd1aeaeee4179b1a9
naotoAkashi42 <71385466+naotoAkashi42@users.noreply.github.com>
2021-03-07 21:20:27 +0900

    Add files via upload

---
+++ Utilities/Utilities/Utilities.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Utilities
+    {
+        public static IEnumerable<string> SortByStrLen(List<string> list)
+        {
+            list.Sort((a, b) => b.Length - a.Length);
+
+            return list;
+        }
+
+        public static void CompressToGz(DirectoryInfo directorySelected)
+        {
+            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+            {
+                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                {
+                    if ((File.GetAttributes(fileToCompress.FullName) &
+                       FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                    {
+                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                        {
+                            using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                               CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
+                            }
+                        }
+                        FileInfo info = new FileInfo(Path.DirectorySeparatorChar + fileToCompress.Name + ".gz");
+                        Console.WriteLine($"Compressed {fileToCompress.Name} from {fileToCompress.Length.ToString()} to {info.Length.ToString()} bytes.");
+                    }
+                }
+            }
+        }
+
+        public static void DecompressGz(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Console.WriteLine($"Decompressed: {fileToDecompress.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly CsvImportExport _csvImportExport;
+
+        private IEnumerable<string> Headers => new List<string> {"基本" + Environment.NewLine + "開始,条件,終了,条件", "応用" };
+
+        private IEnumerable<string> BasicSettings => new List<string>
+        {
+            "MR000,↑,MR001,↓"
+        };
+
+        private IEnumerable<string> AppSetting => new List<string>
+        {
+            "aaa,bbb", 
+            "ccc,ddd"
+        };
+
+        public Form1()
+        {
+            _csvImportExport = new CsvImportExport(Headers);
+            InitializeComponent();
+        }
+
+        public void Export()
+        {
+            _csvImportExport.SetCsvSettings(Headers.ToList()[0], BasicSettings);
+            _csvImportExport.SetCsvSettings(Headers.ToList()[1], AppSetting);
+            _csvImportExport.Export();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Export();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _csvImportExport.Import();
+        }
+    }
+}
